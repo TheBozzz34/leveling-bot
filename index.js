4aa3bdee5c0a1749efd7be860ed71d86eef405b3
Ethan <lskywalker416@gmail.com>
2022-09-23 20:21:30 -0600

    switch to command handler

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const chalk = require('chalk');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// eslint-disable-next-line no-unused-vars
-const { chalk } = require('chalk');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const pino = require('pino');
 const logger = pino();
 module.exports = logger;
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
-	logger.info('Logged in as: '.underline.green);
+	logger.info(chalk.blue('Logged in as: %s#%s'), client.user.username, client.user.discriminator);
 });
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
-	else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	}
-	else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
