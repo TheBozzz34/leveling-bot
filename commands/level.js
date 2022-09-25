396da43ad0e4558d13ea272cf4e1b011249771ca
Ethan <lskywalker416@gmail.com>
2022-09-25 12:24:09 -0600

    + ability to check another users level

--- commands/level.js
+++ commands/level.js
@@ -7,24 +7,41 @@ Levels.setURL(mongoURL);
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('level')
-		.setDescription('Gets your current user level'),
+		.setDescription('Gets your current user level')
+		.addUserOption(option => option.setName('user').setDescription('user')),
 	async execute(interaction) {
 		// const level = Levels.fetch(interaction.user.id, '771017297816846387', false);
 		// console.log(level);
 		// await interaction.reply({ content: 'logged', ephemeral: true });
-		const level = await Levels.fetch(interaction.user.id, interaction.guild.id, true);
+		const user = interaction.options.getUser('user');
+		if (!user) {
+			const level = await Levels.fetch(interaction.user.id, interaction.guild.id, true);
+			const nextXp = Levels.xpFor(level.level + 1);
+			const xpNeeded = nextXp - level.xp;
+			const embed = new EmbedBuilder()
+				.setTitle(`${interaction.user.username}'s level`)
+				.setColor(randomHex.generate())
+				.setDescription(`You are currently level **${level.level}!**`)
+				.setFooter({
+					text: `You have ${level.xp} XP and need ` + xpNeeded + ' XP to level up!',
+					iconURL: interaction.user.displayAvatarURL(),
+				});
+			// console.log(`You have ${level.xp} XP and need ` + xpNeeded + ' XP to level up!');
+			return await interaction.reply({ embeds: [embed] });
+		}
+		const level = await Levels.fetch(user.id, interaction.guild.id, true);
 		const nextXp = Levels.xpFor(level.level + 1);
 		const xpNeeded = nextXp - level.xp;
 		const embed = new EmbedBuilder()
-			.setTitle(`${interaction.user.username}'s level`)
+			.setTitle(`${user.username}'s level`)
 			.setColor(randomHex.generate())
-			.setDescription(`You are currently level **${level.level}!**`)
+			.setDescription(`${user.username} is currently level **${level.level}!**`)
 			.setFooter({
-				text: `You have ${level.xp} XP and need ` + xpNeeded + ' XP to level up!',
-				iconURL: interaction.user.displayAvatarURL(),
+				text: `${user.username} hase ${level.xp} XP and needs ` + xpNeeded + ' XP to level up!',
+				iconURL: user.displayAvatarURL(),
 			});
 		// console.log(`You have ${level.xp} XP and need ` + xpNeeded + ' XP to level up!');
-		interaction.reply({ embeds: [embed] });
+		await interaction.reply({ embeds: [embed] });
 		// await interaction.reply({ content: `Your current level is ${level.level}!`, ephemeral: true });
 	},
 };
